fc789b9df08adb7df6220aeb5308a4241b5078a1
katja <katja@KATJA>
2026-09-24 20:13:38 +0200

    Add API endpoints for searching, sorting and filtering

--- Backend/src/main/java/com/github/kv20230/backend/controller/CountryController.java
+++ Backend/src/main/java/com/github/kv20230/backend/controller/CountryController.java
@@ -4,6 +4,7 @@ import com.github.kv20230.backend.model.dto.Country;
 import com.github.kv20230.backend.model.dto.RestCountryResponse;
 import com.github.kv20230.backend.service.CountryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -49,6 +50,103 @@ public class CountryController {
     //returns a list of all countries
     public List<Country> getAllCountries() {
         return countryService.getAllCountries();
+    }
+
+    @Operation(
+            summary = "Sort countries by a chosen region.",
+            description = "Retrieves a list of all countries in a certain region."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully sorted countries by region"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Region not found",
+                    content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+            )
+    })
+    @GetMapping("/regions")
+    public List<Country> getCountriesByRegion (
+            @Parameter(description = "Name of the region (e.g. Europe, Americas, Asia, Africa, Oceania)", example = "Europe")
+            @RequestParam String region) {
+        List<Country> cachedCountries = countryService.getAllCountries();
+        return countryService.getCountriesByRegion(cachedCountries, region);
+    }
+
+    @Operation(
+            summary = "Sorts countries by population.",
+            description = "Retrieves a list that is sorted by population ascending or descending."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully sorted countries by population"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+            )
+    })
+    @GetMapping("/population")
+    public List<Country> sortCountriesByPopulation(
+            @Parameter(description = "The countries can either be sorted population ascending or descending", example = "ascending")
+            @RequestParam String direction) {
+        List<Country> cachedCountries = countryService.getAllCountries();
+        return countryService.sortCountriesByPopulation(cachedCountries, direction);
+    }
+
+    @Operation(
+            summary = "Retrieves a country.",
+            description = "Retrieves a country that matches the given 3 letter code."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieves the country."
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+            )
+    })
+    @GetMapping("/{alpha3Code}")
+    public Country getCountryByCode(
+            @Parameter(description = "Unique 3 letter code for the country", example = "SVN")
+            @PathVariable String alpha3Code) {
+        List<Country> cachedCountries = countryService.getAllCountries();
+        return countryService.getCountryByCode(cachedCountries, alpha3Code);
+    }
+
+    @Operation(
+            summary = "Retrieves a country.",
+            description = "Retrieves a country that matches the given name."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieves the country."
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+            )
+    })
+    @GetMapping("/names")
+    public List<Country> getCountriesByName(
+            @Parameter(description = "Name of the country, either official or common", example = "Slovenia")
+            @RequestParam String name) {
+        List<Country> cachedCountries = countryService.getAllCountries();
+        return countryService.getCountriesByName(cachedCountries, name);
     }
 
 
--- Backend/src/main/java/com/github/kv20230/backend/service/CountryService.java
+++ Backend/src/main/java/com/github/kv20230/backend/service/CountryService.java
@@ -3,9 +3,12 @@ package com.github.kv20230.backend.service;
 import com.github.kv20230.backend.client.RestCountryClient;
 import com.github.kv20230.backend.mapper.CountryMapper;
 import com.github.kv20230.backend.model.dto.Country;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -19,11 +22,68 @@ public class CountryService {
         this.countryMapper = countryMapper;
     }
 
-    //Metoda, ki pokliče zunanji API (REST Countries) nato pa podatke shrani v medpomnilnik
+    //Function that call external REST Country API until it gets all countries and caches the results.
     @Cacheable("countries")
     public List<Country> getAllCountries() {
         return restCountryClient.fetchAllCountries().stream()
                 .map(countryMapper::toResponseDto)
+                .toList();
+    }
+
+    public List<Country> getCountriesByRegion(List<Country> cachedCountries, String region) {
+        if (region == null || region.isBlank()) {
+            return cachedCountries;
+        }
+        return cachedCountries.stream()
+                .filter(c -> c.region() != null && c.region().equalsIgnoreCase(region.trim()))
+                .toList();
+    }
+
+    public List<Country> sortCountriesByPopulation(List<Country> cachedCountries, String direction) {
+        if (direction == null || direction.isBlank()) {
+            return cachedCountries;
+        }
+
+        //default: ascending
+        Comparator<Country> comparator = Comparator.comparingLong(
+                c -> c.population() != null ? c.population() : 0L
+        );
+
+        //descending
+        if ("desc".equalsIgnoreCase(direction)) {
+            comparator = comparator.reversed();
+        }
+
+        return  cachedCountries.stream()
+                .sorted(comparator)
+                .toList();
+    }
+
+    public Country getCountryByCode(List<Country> cachedCountries, String alpha3Code) {
+        if (alpha3Code == null || alpha3Code.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Country not found with code: " + alpha3Code);
+        }
+
+        String normalizedCode = alpha3Code.trim();
+
+        return cachedCountries.stream()
+                .filter(c -> c.alpha3Code() != null && c.alpha3Code().equalsIgnoreCase(normalizedCode))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Country not found with code: " + alpha3Code));
+    }
+
+    public List<Country> getCountriesByName(List<Country> cachedCountries, String name) {
+        if (name == null || name.isBlank()) {
+            return cachedCountries;
+        }
+
+        String query = name.trim().toLowerCase();
+
+        return cachedCountries.stream()
+                .filter(c -> (c.commonName() != null && c.commonName().toLowerCase().contains(query))
+                        || (c.officialName() != null && c.officialName().toLowerCase().contains(query)))
                 .toList();
     }
 }
